c1d4cec2cffe860c2da1581359c1e203785b8977
unknown <Andisyc@163.com>
2026-03-15 22:06:12 +0800

    update commit

--- deploy/start.sh
+++ deploy/start.sh
@@ -9,19 +9,19 @@
 #     mimic.motion.motion_path=/path/to/motion/path
 
 # HYDRA_FULL_ERROR=1 python run.py --config-name=eval \
-#     mimic.policy.checkpoint=./data/model/gmt.onnx \
-#     mimic.policy.use_estimator=False \
-#     robot.control.viewer=False \
-#     robot.control.real_time=False \
-#     mimic.motion.motion_path=./data/motion/dance1_subject1.npz \
-#     mimic.policy.history_length=5 \
-#     mimic.policy.eval_mode=True \
-#     mimic.motion.command_horizon=1
+    mimic.policy.checkpoint=./data/model/gmt.onnx \
+    mimic.policy.use_estimator=False \
+    robot.control.viewer=False \
+    robot.control.real_time=False \
+    mimic.motion.motion_path=./data/motion/dance1_subject1.npz \
+    mimic.policy.history_length=5 \
+    mimic.policy.eval_mode=True \
+    mimic.motion.command_horizon=1
 
 # GMT Evaluation
-HYDRA_FULL_ERROR=1 export LIBGL_ALWAYS_SOFTWARE=1 && python run.py --config-name=gmt sim=mujoco robot=g1_29dof \
-    mimic.policy.checkpoint=./data/model/gmt.onnx \
-    robot.control.viewer=True \
-    robot.control.real_time=True \
-    mimic.motion.loop=True \
-    mimic.motion.motion_path=./data/motion/dance1_subject1.npz
+# HYDRA_FULL_ERROR=1 export LIBGL_ALWAYS_SOFTWARE=1 && python run.py --config-name=gmt sim=mujoco robot=g1_29dof \
+#     mimic.policy.checkpoint=./data/model/gmt.onnx \
+#     robot.control.viewer=True \
+#     robot.control.real_time=True \
+#     mimic.motion.loop=True \
+#     mimic.motion.motion_path=./data/motion/dance1_subject1.npz
