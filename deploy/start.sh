a406db772f8d6085e51bd755d0d0e158ebf88315
Andisyc <Andisyc@163.com>
2026-05-14 19:51:23 +0800

    sync update

--- deploy/start.sh
+++ deploy/start.sh
@@ -7,6 +7,15 @@
 #     mimic.motion.motion_path=./data/motion/dance1_subject1.npz
 
 # GMT Evaluation: config-name has to be mosaic (been tested)
-HYDRA_FULL_ERROR=1 export LIBGL_ALWAYS_SOFTWARE=1 && python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
-    ++robot.control.viewer=True \
-    ++robot.control.real_time=True \
+# HYDRA_FULL_ERROR=1 export LIBGL_ALWAYS_SOFTWARE=1 && python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
+#     ++robot.control.viewer=True \
+#     ++robot.control.real_time=True \
+
+HYDRA_FULL_ERROR=1 LIBGL_ALWAYS_SOFTWARE=1 python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
+  ++robot.control.viewer=True \
+  ++robot.control.real_time=True \
+  env.config.record_video.enabled=True \
+  env.config.record_video.output_dir=./videos_mosaic \
+  env.config.record_video.fps=30 \
+  env.config.record_video.width=1280 \
+  env.config.record_video.height=720
