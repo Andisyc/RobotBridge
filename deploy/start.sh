a0e62310e8a16ec69e6dffd81d0b74a23c727a8d
Andisyc <Andisyc@163.com>
2026-05-14 20:10:42 +0800

    sync update

--- deploy/start.sh
+++ deploy/start.sh
@@ -11,10 +11,14 @@
 #     ++robot.control.viewer=True \
 #     ++robot.control.real_time=True \
 
-HYDRA_FULL_ERROR=1 LIBGL_ALWAYS_SOFTWARE=1 python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
-  ++robot.control.viewer=True \
-  ++robot.control.real_time=True \
+
+# GMT Record mp4
+HYDRA_FULL_ERROR=1 MUJOCO_GL=egl PYOPENGL_PLATFORM=egl python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
+  ++robot.control.viewer=False \
+  ++robot.control.real_time=False \
+  mimic.motion.loop=False \
   env.config.record_video.enabled=True \
+  env.config.record_video.exit_on_complete=True \
   env.config.record_video.output_dir=./videos_mosaic \
   env.config.record_video.fps=30 \
   env.config.record_video.width=1280 \
