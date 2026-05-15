19b860f6abf6ded24307b27fe257e720af76f1bd
Andisyc <Andisyc@163.com>
2026-05-15 14:11:28 +0800

    sync update

--- deploy/start.sh
+++ deploy/start.sh
@@ -11,8 +11,7 @@
 #     ++robot.control.viewer=True \
 #     ++robot.control.real_time=True \
 
-
-# GMT Record mp4
+# GMT Record mp4 filter trackable motion sequence
 # HYDRA_FULL_ERROR=1 MUJOCO_GL=egl PYOPENGL_PLATFORM=egl python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
 #   ++robot.control.viewer=False \
 #   ++robot.control.real_time=False \
@@ -24,11 +23,12 @@
 #   env.config.record_video.width=1280 \
 #   env.config.record_video.height=720
 
-HYDRA_FULL_ERROR=1 python scripts/robustness_validation/run_validation_mujoco.py \
+# GMT Record mp4 single motion sequence
+HYDRA_FULL_ERROR=1 MUJOCO_GL=egl PYOPENGL_PLATFORM=egl python robustness_validation/run_validation_mujoco.py \
   --robotbridge_root /home/chengyuxuan/RobotBridge \
-  --motion "/home/chengyuxuan/RobotBridge/deploy/data/motion/Walking/amass_g1_jump-06-rope normal run fast-kudo_poses.npz" \
-  --checkpoint /home/chengyuxuan/RobotBridge/deploy/data/model/model_27000.onnx \
-  --output_dir verify/robustness_validation_mujoco/smoke \
+  --motion "./data/motion/Walking/amass_g1_jump-06-rope normal run fast-kudo_poses.npz" \
+  --checkpoint ./data/model/model_27000.onnx \
+  --output_dir ./verify/robustness_validation_mujoco/smoke \
   --epsilon_values 0.0 0.02 \
   --push_velocities 0.0 1.0 \
   --num_trials 2 \
