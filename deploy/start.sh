0fc2c3ec71eebd85f032098e4a86d63c342e03f9
Andisyc <Andisyc@163.com>
2026-05-15 13:51:19 +0800

    sync update

--- deploy/start.sh
+++ deploy/start.sh
@@ -13,13 +13,24 @@
 
 
 # GMT Record mp4
-HYDRA_FULL_ERROR=1 MUJOCO_GL=egl PYOPENGL_PLATFORM=egl python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
-  ++robot.control.viewer=False \
-  ++robot.control.real_time=False \
-  mimic.motion.loop=False \
-  env.config.record_video.enabled=True \
-  env.config.record_video.exit_on_complete=True \
-  env.config.record_video.output_dir=./videos_mosaic \
-  env.config.record_video.fps=30 \
-  env.config.record_video.width=1280 \
-  env.config.record_video.height=720
+# HYDRA_FULL_ERROR=1 MUJOCO_GL=egl PYOPENGL_PLATFORM=egl python run.py --config-name=mosaic sim=mujoco robot=g1_29dof \
+#   ++robot.control.viewer=False \
+#   ++robot.control.real_time=False \
+#   mimic.motion.loop=False \
+#   env.config.record_video.enabled=True \
+#   env.config.record_video.exit_on_complete=True \
+#   env.config.record_video.output_dir=./videos \
+#   env.config.record_video.fps=30 \
+#   env.config.record_video.width=1280 \
+#   env.config.record_video.height=720
+
+HYDRA_FULL_ERROR=1 python scripts/robustness_validation/run_validation_mujoco.py \
+  --robotbridge_root /home/chengyuxuan/RobotBridge \
+  --motion "/home/chengyuxuan/RobotBridge/deploy/data/motion/Walking/amass_g1_jump-06-rope normal run fast-kudo_poses.npz" \
+  --checkpoint /home/chengyuxuan/RobotBridge/deploy/data/model/model_27000.onnx \
+  --output_dir verify/robustness_validation_mujoco/smoke \
+  --epsilon_values 0.0 0.02 \
+  --push_velocities 0.0 1.0 \
+  --num_trials 2 \
+  --perturbation_modes composite \
+  --record_video
